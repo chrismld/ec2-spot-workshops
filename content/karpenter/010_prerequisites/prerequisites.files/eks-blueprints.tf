2c1068ec7a1587635a22cfd2dc1b32158c4009d5
Christian Melendez <chrismld@amazon.com>
2023-08-30 13:11:55 +0200

    Added the installation of kube-ops-view

--- content/karpenter/010_prerequisites/prerequisites.files/eks-blueprints.tf
+++ content/karpenter/010_prerequisites/prerequisites.files/eks-blueprints.tf
@@ -222,6 +222,143 @@ module "vpc" {
   tags = local.tags
 }
 
+#---------------------------------------------------------------
+# Kubernetes Manifests
+#---------------------------------------------------------------
+
+resource "kubectl_manifest" "kube_ops_view_deployment" {
+  yaml_body = <<-YAML
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      labels:
+        application: kube-ops-view
+        component: frontend
+      name: kube-ops-view
+    spec:
+      replicas: 1
+      selector:
+        matchLabels:
+          application: kube-ops-view
+          component: frontend
+      template:
+        metadata:
+          labels:
+            application: kube-ops-view
+            component: frontend
+        spec:
+          nodeSelector:
+            intent: control-apps
+          serviceAccountName: kube-ops-view
+          containers:
+          - name: service
+            image: hjacobs/kube-ops-view:20.4.0
+            ports:
+            - containerPort: 8080
+              protocol: TCP
+            readinessProbe:
+              httpGet:
+                path: /health
+                port: 8080
+              initialDelaySeconds: 5
+              timeoutSeconds: 1
+            livenessProbe:
+              httpGet:
+                path: /health
+                port: 8080
+              initialDelaySeconds: 30
+              periodSeconds: 30
+              timeoutSeconds: 10
+              failureThreshold: 5
+            resources:
+              limits:
+                cpu: 400m
+                memory: 400Mi
+              requests:
+                cpu: 400m
+                memory: 400Mi
+            securityContext:
+              readOnlyRootFilesystem: true
+              runAsNonRoot: true
+              runAsUser: 1000
+  YAML
+
+  depends_on = [
+    module.eks_blueprints_addons
+  ]
+}
+
+resource "kubectl_manifest" "kube_ops_view_rbac" {
+  yaml_body = <<-YAML
+    ---
+    apiVersion: v1
+    kind: ServiceAccount
+    metadata:
+      name: kube-ops-view
+    ---
+    kind: ClusterRole
+    apiVersion: rbac.authorization.k8s.io/v1
+    metadata:
+      name: kube-ops-view
+    rules:
+    - apiGroups: [""]
+      resources: ["nodes", "pods"]
+      verbs:
+        - list
+    - apiGroups: ["metrics.k8s.io"]
+      resources: ["nodes", "pods"]
+      verbs:
+        - get
+        - list
+    ---
+    kind: ClusterRoleBinding
+    apiVersion: rbac.authorization.k8s.io/v1
+    metadata:
+      name: kube-ops-view
+    roleRef:
+      apiGroup: rbac.authorization.k8s.io
+      kind: ClusterRole
+      name: kube-ops-view
+    subjects:
+    - kind: ServiceAccount
+      name: kube-ops-view
+      namespace: default
+  YAML
+
+  depends_on = [
+    module.eks_blueprints_addons
+  ]
+}
+
+resource "kubectl_manifest" "kube_ops_view_service" {
+  yaml_body = <<-YAML
+    apiVersion: v1
+    kind: Service
+    metadata:
+      labels:
+        application: kube-ops-view
+        component: frontend
+      name: kube-ops-view
+    spec:
+      selector:
+        application: kube-ops-view
+        component: frontend
+      type: LoadBalancer
+      ports:
+      - port: 80
+        protocol: TCP
+        targetPort: 8080
+  YAML
+
+  depends_on = [
+    module.eks_blueprints_addons
+  ]
+}
+
+#---------------------------------------------------------------
+# Outputs
+#---------------------------------------------------------------
+
 output "configure_kubectl" {
   description = "Configure kubectl: make sure you're logged in with the correct AWS profile and run the following command to update your kubeconfig"
   value       = "aws eks --region ${local.region} update-kubeconfig --name ${module.eks.cluster_name}"
